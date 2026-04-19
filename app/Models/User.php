e42e15ab7f80abe2793cf8a1089451a9b906ca2b
hiqmal7 <hiqiimall@gmail.com>
2026-04-19 19:46:22 +0700

    Menambahkan model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,47 +3,41 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use function Pest\Laravel\get;
+
+#[Fillable(['username', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    use Notifiable;
+ 
+    protected $primaryKey = 'npm';
+    public $incrementing  = false;  // npm bukan auto-increment
+    protected $keyType    = 'int';
+ 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'npm', 'username', 'first_name', 'last_name',
+        'email', 'email_verified_at', 'password',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
+ 
+    protected $hidden = ['password'];
+ 
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+ 
+    // Satu user bisa punya banyak pinjaman
+    public function loans()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(loans::class, 'user_npm', 'npm');
     }
 }
